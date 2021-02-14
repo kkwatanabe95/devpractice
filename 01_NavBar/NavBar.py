569e006422d63b98b4b9eb04d2d8769cbc9f09ce
kkwatanabe95 <60697728+kkwatanabe95@users.noreply.github.com>
2021-02-14 14:01:03 +0900

    Just getting Started

---
+++ 01_NavBar/NavBar.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__)
+#To solve issue of browser caching CSS stylesheet data that was causing changes to the stylesheet to not be reflected in page.
+#Can also be solved by hard reloading the browser (Ctrl + shift + R)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0		
+
+
+@app.route("/home/")
+def home():
+	return render_template("home.html")
+
+@app.route("/flexbox/")
+def flexbox():
+	return render_template("flexbox.html")
+
+@app.route("/toggle/")
+def toggle():
+	return render_template("toggle.html")
+
+if __name__ == "__main__":
+	    app.run(debug=True)
