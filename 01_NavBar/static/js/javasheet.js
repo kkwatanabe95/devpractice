24463285b54dae680cb1a2a2e80ee6b821e05c96
kkwatanabe95 <60697728+kkwatanabe95@users.noreply.github.com>
2021-02-20 15:29:21 +0900

    bgPhoto flexbox and starting to try toggle

---
+++ 01_NavBar/static/js/javasheet.js
@@ -0,0 +1,7 @@
+function toggleForest() {
+	document.getElementById('navbarid').className = "navbartoggle";
+}
+
+function defaultstyle() {
+	document.getElementById('navbarid').className = "navbar";
+}
