b4d16edb45cc0d880bd8e3e5948d77c1592ef96a
kkwatanabe95 <60697728+kkwatanabe95@users.noreply.github.com>
2021-02-21 09:58:24 +0900

    toggle style switch

--- 01_NavBar/NavBar.py
+++ 01_NavBar/NavBar.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, send_from_directory
 app = Flask(__name__)
 #To solve issue of browser caching CSS stylesheet data that was causing changes to the stylesheet to not be reflected in page.
 #Can also be solved by hard reloading the browser (Ctrl + shift + R)
